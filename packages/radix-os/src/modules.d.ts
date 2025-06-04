0a083979cc296d121f048c74c04bc9a6fe9b8f55
Håkon <haakonu97@gmail.com>
2025-06-04 21:22:16 +0200

    version: 0.2.57

--- packages/radix-os/src/modules.d.ts
+++ packages/radix-os/src/modules.d.ts
@@ -1,10 +1,3 @@
-import "csstype";
-
-declare module "csstype" {
-  interface Properties {
-    [index: `--${string}`]: string | number;
-  }
-}
 declare module "*.module.css" {
   const classes: { [key: string]: string };
   export default classes;
